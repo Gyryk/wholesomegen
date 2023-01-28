19e6826b832feba8e1a4e910393cc124fbca007e
Girik <68961075+Gyryk@users.noreply.github.com>
2023-01-28 12:11:45 +0530

    Update getPics.js

--- getPics.js
+++ getPics.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 // Create Write Stream
 var WriteStream = fs.createWriteStream("ImagesLink.txt", "UTF-8");
 
-// Send Request
+// Send Request 1
 request("https://paradepets.com/pet-news/cute-animals", (err, resp, html)=>
 {
     if(!err && resp.statusCode == 200)
@@ -27,5 +27,29 @@ request("https://paradepets.com/pet-news/cute-animals", (err, resp, html)=>
     else
     {
         console.log("Request failed");
+    }
+})
+
+// Send Request 2
+request("https://www.boredpanda.com/cute-baby-animals/", (err, resp, html)=>
+{
+    if(!err && resp.statusCode == 200)
+    {
+        console.log("Request successful");
+
+        const $ =cherio.load(html);
+
+        $("img").each((index, image)=>{
+            var img = $(image).attr('src');
+            var baseUrl = "https://www.boredpanda.com";
+            var Links = img;
+
+            WriteStream.write(Links);
+            WriteStream.write("\n");
+        })
+    }
+    else
+    {
+        console.log("Request failed")
     }
 })
